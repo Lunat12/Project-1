52c4be1b3c18d8e31791c06d993cd3c22a702eb0
Lunat12 <nati12n@gmail.com>
2021-11-24 12:42:54 +0100

    remaned

--- Strings/Challenge2_NataliaMuñiz.py
+++
@@ -1,4 +0,0 @@
-print('Welcome to the Miles Per Hour Conversion App')
-miles = float(input('What is your speed in miles per hour: '))
-mps = round(miles * 0.4474, 2)
-print(f'Your speed in meters per second is {mps}')
