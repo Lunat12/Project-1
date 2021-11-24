0624cca9b40c76815221364a5459e42ddff966df
Lunat12 <nati12n@gmail.com>
2021-11-24 10:18:26 +0100

    now with folders!

---
+++ Strings/Challenge1_NataliaMuñiz.py
@@ -0,0 +1,2 @@
+userName = input("enter user name: ")
+print(f'{userName} tiene {len(userName)} letras.')
---
+++ Strings/Challenge2_NataliaMuñiz.py
@@ -0,0 +1,4 @@
+print('Welcome to the Miles Per Hour Conversion App')
+miles = float(input('What is your speed in miles per hour: '))
+mps = round(miles * 0.4474, 2)
+print(f'Your speed in meters per second is {mps}')
